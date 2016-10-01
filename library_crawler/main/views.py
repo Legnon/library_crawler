65390a606fe993e27041b064a1f591b5ea6a4148
KongHyeontaek <rotanev7@gmail.com>
2016-10-01 19:52:07 +0900

    성능 개선
    thread를 이용한 성능 개선

--- library_crawler/main/views.py
+++ library_crawler/main/views.py
@@ -4,45 +4,32 @@ import os
 import mechanicalsoup
 import urllib.parse as urlparse
 from urllib.parse import urlencode
+import threading
 
 
-def main(request, *args, **kwargs):
-    # context = {}
-    context_str = ""
+class MyThread(threading.Thread):
+    def __init__(self, threadid, name, counter, room_list, date):
+        threading.Thread.__init__(self)
+        self.threadID = threadid
+        self.name = name
+        self.counter = counter
+        self.room_list = room_list
+        self.date = date
+
+    def run(self):
+        self.result_str = get_result(self.room_list, self.date)
+
+    def join(self):
+        threading.Thread.join(self)
+        return self.result_str
+
+
+def get_result(room_list, date):
+    result_str = ""
+
+    page_url = 'http://library.hanyang.ac.kr/studyroom/main'
 
     browser = mechanicalsoup.Browser()
-
-    room_list = []
-    if 'room' in kwargs.keys():
-        if kwargs['room'] == "1":
-            for i in range(45, 49):
-                room_list += [i]
-        elif kwargs['room'] == "2":
-            for i in range(49, 53):
-                room_list += [i]
-        elif kwargs['room'] == "3":
-            for i in range(63, 73):
-                room_list += [i]
-        else:
-            return HttpResponse('1,2,3 중 하나만 입력')
-    else:
-        for i in range(45, 53):
-            room_list += [i]
-        for i in range(63, 73):
-            room_list += [i]
-
-    if 'date' in kwargs.keys():
-        if len(kwargs['date']) == 4:
-            date = datetime.datetime.today().strftime('%Y') + kwargs['date']
-        elif len(kwargs['date']) == 8:
-            date = kwargs['date']
-        else:
-            return HttpResponse('yyyymmdd 혹은 mmdd 식으로 입력')
-    else:
-        date = datetime.datetime.today().strftime('%Y%m%d')
-
-    # main url
-    page_url = 'http://library.hanyang.ac.kr/studyroom/main'
 
     for rid in room_list:
         params = {
@@ -73,12 +60,67 @@ def main(request, *args, **kwargs):
 
             # 예약 가능일이 아니거나 공휴일인 경우는 바로 리턴
             if html.soup.find(class_='noticeW').find('span', style="font: bold; color: red;"):
-                return HttpResponse("예약 가능일이 아님")
+                return "예약 가능일이 아님"
             elif html.soup.find(class_='listtable').find('tbody').text.strip() == "":
-                return HttpResponse("공휴일은 스터디룸이 없음")
+                return "공휴일은 스터디룸이 없음"
 
-        context_str += str(rid) + "<br>" + str(html.soup.find(class_='listtable'))
-        # context.update({str(rid): str(html.soup.find(class_='listtable'))})
+        result_str += str(rid) + "<br>" + str(html.soup.find(class_='listtable')) + "<br><br>"
 
-    return HttpResponse(context_str)
-    # return render(request, 'main/main.html', context)
+    return result_str
+
+
+def main(request, *args, **kwargs):
+    print(datetime.datetime.now())
+
+    room_list = []
+    if 'room' in kwargs.keys():
+        if kwargs['room'] == "1":
+            for i in range(45, 49):
+                room_list += [i]
+        elif kwargs['room'] == "2":
+            for i in range(49, 53):
+                room_list += [i]
+        elif kwargs['room'] == "3":
+            for i in range(63, 73):
+                room_list += [i]
+        else:
+            return HttpResponse('1,2,3 중 하나만 입력')
+    else:
+        for i in range(45, 53):
+            room_list += [i]
+        for i in range(63, 73):
+            room_list += [i]
+
+    if 'date' in kwargs.keys():
+        if len(kwargs['date']) == 4:
+            date = datetime.datetime.today().strftime('%Y') + kwargs['date']
+        elif len(kwargs['date']) == 8:
+            date = kwargs['date']
+        else:
+            return HttpResponse('yyyymmdd 혹은 mmdd 식으로 입력')
+    else:
+        date = datetime.datetime.today().strftime('%Y%m%d')
+
+    thread1 = MyThread(1, "Thread-1", 1, room_list[0::4], date)
+    thread2 = MyThread(2, "Thread-2", 2, room_list[1::4], date)
+    thread3 = MyThread(3, "Thread-3", 3, room_list[2::4], date)
+    thread4 = MyThread(4, "Thread-4", 4, room_list[3::4], date)
+
+    thread1.start()
+    thread2.start()
+    thread3.start()
+    thread4.start()
+
+    result_str1 = thread1.join()
+    result_str2 = thread2.join()
+    result_str3 = thread3.join()
+    result_str4 = thread4.join()
+
+    print(datetime.datetime.now())
+
+    if result_str1 == "예약 가능일이 아님" or result_str1 == "공휴일은 스터디룸이 없음":
+        final_str = result_str1
+    else:
+        final_str = result_str1 + result_str2 + result_str3 + result_str4
+
+    return HttpResponse(final_str)
